cd56814e7376a9a313288136dda847d1075a57ac
cbhoomika21-hue <cbhoomika21@gmail.com>
2026-03-06 12:12:23 +0530

    Time: 1 ms (97.33%), Space: 42.3 MB (95.58%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,10 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0) return 0;
-        int i=1;
-        while((long)i*i<=x){
-            i++;
+        int left = 1, right = x, ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return i-1;
+        return ans;
     }
+
 }
