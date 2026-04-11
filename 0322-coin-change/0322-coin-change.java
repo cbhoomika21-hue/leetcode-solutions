29695524646a77772d6cd4973627b570ff085188
cbhoomika21-hue <cbhoomika21@gmail.com>
2026-04-11 15:04:36 +0530

    Time: 15 ms (82.13%), Space: 46 MB (96.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,12 +2,11 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
 
-        // initialize with a large value
         for (int i = 0; i <= amount; i++) {
             dp[i] = amount + 1;
         }
 
-        dp[0] = 0; // base case
+        dp[0] = 0; 
 
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
