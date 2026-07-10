b02796b40d8b47338ca0181e7cf7d7c1a8ae2786
cbhoomika21-hue <cbhoomika21@gmail.com>
2026-07-10 17:01:47 +0530

    Time: 6 ms (99.16%), Space: 76.3 MB (73.43%) - LeetHub

---
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.java
@@ -0,0 +1,37 @@
+class Solution {
+        public int minMoves(int[] nums, int limit) {
+                int n = nums.length;
+                        // The possible sums range from 2 to 2 * limit
+                                int[] diff = new int[2 * limit + 2];
+                                        
+                                                for (int i = 0; i < n / 2; i++) {
+                                                            int a = nums[i];
+                                                                        int b = nums[n - 1 - i];
+                                                                                    
+                                                                                                int sum = a + b;
+                                                                                                            int minSum = Math.min(a, b) + 1;
+                                                                                                                        int maxSum = Math.max(a, b) + limit;
+                                                                                                                                    
+                                                                                                                                                // Default: 2 moves needed for all target sums
+                                                                                                                                                            diff[2] += 2;
+                                                                                                                                                                        diff[2 * limit + 1] -= 2;
+                                                                                                                                                                                    
+                                                                                                                                                                                                // 1 move needed for range [minSum, maxSum]
+                                                                                                                                                                                                            diff[minSum] -= 1;
+                                                                                                                                                                                                                        diff[maxSum + 1] += 1;
+                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                // 0 moves needed for target sum = sum
+                                                                                                                                                                                                                                                            diff[sum] -= 1;
+                                                                                                                                                                                                                                                                        diff[sum + 1] += 1;
+                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                int minMoves = n; // Max possible moves is n
+                                                                                                                                                                                                                                                                                                        int currentMoves = 0;
+                                                                                                                                                                                                                                                                                                                for (int i = 2; i <= 2 * limit; i++) {
+                                                                                                                                                                                                                                                                                                                            currentMoves += diff[i];
+                                                                                                                                                                                                                                                                                                                                        minMoves = Math.min(minMoves, currentMoves);
+                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                return minMoves;
+                                                                                                                                                                                                                                                                                                                                                                    }
+}
